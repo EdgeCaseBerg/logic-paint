a97a757445a51a817981aaac5a51a6c5ea47c3af
peetseater <peetseater@gmail.com>
2026-01-17 12:53:50 -0500

    Initial parse of PBM file (working)

--- src/netbpm.rs
+++ src/netbpm.rs
@@ -7,32 +7,87 @@ pub struct PBM {
     pub cells: Vec<bool>,
 }
 
-impl BPM {
-	pub fn from_ascii(string: &str) -> BPM {
-		BPM {
-			width: 0,
-			height: 0,
-			cells: Vec::new()
-		}
-	}
+#[derive(Debug)]
+pub enum LoadPbmResult {
+    Loaded(PBM),
+    HeaderError(String),
+    MissingSizeError,
+    InvalidSizeError(String),
 }
 
+impl PBM {
+    pub fn from_ascii(string: &str) -> LoadPbmResult {
+        let mut characters = string.split_whitespace();
+        let flag = characters.next();
+        match flag {
+            Some("P1") => {}
+            Some(bad) => {
+                return LoadPbmResult::HeaderError(
+                    "PBM file did not contain starting header of P1 contains:".to_owned() + bad,
+                );
+            }
+            None => {
+                return LoadPbmResult::HeaderError(
+                    "PBM file did not contain starting header.".to_owned(),
+                );
+            }
+        };
+
+        let width = characters.next();
+        let height = characters.next();
+
+        if width.is_none() || height.is_none() {
+            return LoadPbmResult::MissingSizeError;
+        }
+        let (width, height): (u16, u16) = match (width.unwrap().parse(), height.unwrap().parse()) {
+            (Ok(w), Ok(h)) => (w, h),
+            (res1, res2) => {
+                let error_string = format!("{:?}, {:?}", res1, res2);
+                return LoadPbmResult::InvalidSizeError(
+                    "Failed to convert size line into u16".to_owned() + &error_string,
+                );
+            }
+        };
+
+        let cells = characters
+            .filter_map(|c| match c {
+                "0" => Some(false),
+                "1" => Some(true),
+                _ => None,
+            })
+            .collect();
+
+        LoadPbmResult::Loaded(PBM {
+            width,
+            height,
+            cells,
+        })
+    }
+}
 
 #[cfg(test)]
-mod bpm_tests {
-	use super::*;
-	#[test]
-	fn can_load_sample_ascii() {
-		let data = read_to_string("assets/P1.pbm").expect("Could not load asset file for test (P1.pbm)");
-		let parsed = BPM::from_ascii(&data);
-		assert_eq!(parsed.width, 5); 
-		assert_eq!(parsed.height, 5); 
-		assert_eq!(parsed.cells, vec![
-			false, true , true, true , false,
-			false, true , true, true , false,
-			false, false, true, false, false,
-			false, true , true, true , false,
-			false, true , true, true , false
-		]);
-	}
+mod pbm_tests {
+    use super::*;
+    #[test]
+    fn can_load_sample_ascii() {
+        let data =
+            read_to_string("assets/P1.pbm").expect("Could not load asset file for test (P1.pbm)");
+
+        if let LoadPbmResult::Loaded(pbm) = PBM::from_ascii(&data) {
+            assert_eq!(pbm.width, 5);
+            assert_eq!(pbm.height, 5);
+            #[rustfmt::skip]
+            {
+            assert_eq!(pbm.cells, vec![
+                false, true , true, true , false,
+                false, true , true, true , false,
+                false, false, true, false, false,
+                false, true , true, true , false,
+                false, true , true, true , false
+            ]);
+            }
+        } else {
+            panic!("Failed to load PBM file, got {:?}", PBM::from_ascii(&data));
+        }
+    }
 }
