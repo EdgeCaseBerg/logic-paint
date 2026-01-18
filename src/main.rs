2f015aac5921833588b06e9a724b9a99cf26bb35
peetseater <peetseater@gmail.com>
2026-01-17 23:55:22 -0500

    Load a custom file in main for fun

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 mod netbpm;
 
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::read_to_string;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let arguments: Vec<String> = env::args().collect();
+    if arguments.is_empty() {
+        eprintln!("{:?}", "pass the input data as the first argument.");
+        return Err("Could not load file".into());
+    }
+
+    let mut arguments = arguments.iter();
+    arguments.next(); // skip the name of the program being ran
+    let filename = match arguments.next() {
+        Some(arg) => arg,
+        _ => "./assets/P1.pbm",
+    };
+
+    let test_pbm = read_to_string(filename)?;
+    let test_pbm: netbpm::Pbm = test_pbm.parse()?;
+    println!("{:?}", test_pbm);
+
+    Ok(())
 }
