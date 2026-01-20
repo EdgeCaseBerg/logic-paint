cea86679a31cacf762317f9a8f5d934d126790f7
peetseater <peetseater@gmail.com>
2026-01-19 22:50:30 -0500

    cargo fmt

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -100,7 +100,9 @@ fn groups(cells: &[Vec<bool>]) -> Vec<Vec<Group>> {
 
 // TODO Should we just use a From?
 // https://youtu.be/gkIpRTq1S6A
-fn groups_from_goal_pairs(state_and_goal_pairs: &[Vec<(PlayerSetState, GoalState)>]) -> Vec<Vec<Group>> {
+fn groups_from_goal_pairs(
+    state_and_goal_pairs: &[Vec<(PlayerSetState, GoalState)>],
+) -> Vec<Vec<Group>> {
     state_and_goal_pairs
         .iter()
         .map(|row| {
