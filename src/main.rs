66f2ea907fdf7f61d90621e07128ac44b87c695e
peetseater <peetseater@gmail.com>
2026-01-25 15:43:14 -0500

    Experimenting with grid generation (poorly)
    Need to swap to actual number of boxes rather than dividing I think

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::env;
 use std::fs::read_to_string;
 
 use egor::{
-    app::{App, FrameContext, WindowEvent, egui::Window},
+    app::{App, FrameContext, WindowEvent, egui::Window, egui::Slider},
     input::{KeyCode, MouseButton},
     math::{Rect, Vec2, vec2},
     render::Color,
@@ -33,8 +33,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{:?}", state);
 
     let mut game_over = false;
+    let mut bg_size = 550;
+    let mut bg_position = vec2(-163., -229.);
+    let mut grid_step = 10;
+    let mut num_boxes = 10;
+    let mut box_offset = 10.0;
 
-    App::new().title("Egor Stateful Rectangle").run(
+    App::new().window_size(1280, 720).title("Logic Brush").run(
         move |FrameContext {
                   gfx,
                   input,
@@ -70,7 +75,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let right_mouse_held = input.mouse_held(MouseButton::Right);
             let right_mouse_released = input.mouse_released(MouseButton::Right);
 
-            gfx.rect().at(world_xy).color(Color::RED);
+            if right_mouse_pressed {
+                bg_position = world_xy;
+                println!("{:?}", bg_position);
+            }
+
+            gfx.rect().at(bg_position).size(Vec2::splat(bg_size as f32)).color(Color::BLUE);
+
+            
+            for r_step in (0..bg_size).step_by(bg_size / num_boxes) {
+                for c_step in (0..bg_size).step_by(bg_size / num_boxes) {
+                    let position = bg_position + Vec2::splat(box_offset/2.) + vec2(r_step as f32, c_step as f32);
+                    gfx.rect().at(position).size(Vec2::splat((bg_size / num_boxes) as f32) - box_offset/2.).color(Color::WHITE);
+                }
+            }
 
             Window::new("Debug").show(egui_ctx, |ui| {
                 ui.label(format!("FPS: {}", timer.fps));
@@ -84,9 +102,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ui.label(format!("Mouse state: right_mouse_pressed: {}", right_mouse_pressed));
                 ui.label(format!("Mouse state: right_mouse_held: {}", right_mouse_held));
                 ui.label(format!("Mouse state: right_mouse_released: {}", right_mouse_released));
+
+                ui.add(Slider::new(&mut bg_size, 1..=800).text("BG size"));
+                ui.add(Slider::new(&mut grid_step, 1..=100).text("Grid step"));
+                ui.add(Slider::new(&mut num_boxes, 10..=20).step_by(5.).text("Grid step"));
+                ui.add(Slider::new(&mut box_offset, 2.0..=20.0).text("Box Offset"));
+                
+                
             });
         },
     );
 
     Ok(())
 }
+
