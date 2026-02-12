ed177cb23dc238b9b8a76fee6f69851bec30b02d
peetseater <peetseater@gmail.com>
2026-02-11 22:36:46 -0500

    Refactor debuggable stuff to persist between frames

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,15 @@ mod netbpm;
 mod screens;
 mod ui;
 
+use crate::gamestate::PlayState;
+use crate::netbpm::Pbm;
+use egor::{
+    app::{FrameContext, egui::ComboBox, egui::Slider, egui::Window},
+    math::Vec2,
+};
 use std::env;
 use std::fs::read_to_string;
+use ui::ColorPalette;
 
 use egor::{
     app::{App, WindowEvent},
@@ -28,6 +35,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let test_pbm = read_to_string(filename)?;
     let test_pbm: netbpm::Pbm = test_pbm.parse()?;
     let mut game_state: gamestate::PlayState = (&test_pbm).into();
+    let mut debuggable_stuff = DebugStuff::new();
+    let mut palette = ColorPalette::meeks();
 
     App::new()
         .window_size(1280, 720)
@@ -45,8 +54,99 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 std::process::exit(0);
             }
 
-            screens::play_game_screen(&mut game_state, frame_context);
+            screens::win_screen(
+                &mut game_state,
+                frame_context,
+                &palette,
+                &mut debuggable_stuff,
+            );
+            // screens::play_game_screen(&mut game_state, frame_context, &mut palette);
+            debug_window(
+                frame_context,
+                &mut debuggable_stuff,
+                &mut palette,
+                &mut game_state,
+            );
         });
 
     Ok(())
+}
+
+struct DebugStuff {
+    size_x: usize,
+    size_y: usize,
+    selected_level: usize,
+}
+
+impl DebugStuff {
+    fn new() -> DebugStuff {
+        DebugStuff {
+            size_x: 200,
+            size_y: 200,
+            selected_level: 0,
+        }
+    }
+}
+
+fn debug_window(
+    frame_context: &mut FrameContext,
+    debuggable_stuff: &mut DebugStuff,
+    palette: &mut ColorPalette,
+    game_state: &mut PlayState,
+) {
+    let gfx = &mut (frame_context.gfx);
+    let input = &mut (frame_context.input);
+    let egui_ctx = frame_context.egui_ctx;
+    let timer = frame_context.timer;
+
+    let screen_size = gfx.screen_size();
+    let (mx, my) = input.mouse_position();
+    let world_xy = gfx.camera().screen_to_world(Vec2::new(mx, my), screen_size);
+
+    let levels = ["./assets/P1.pbm", "./assets/P1-10x10.pbm"];
+
+    Window::new("Debug").show(egui_ctx, |ui| {
+        ui.label(format!("FPS: {}", timer.fps));
+        ui.label(format!("Mouse x: {} y: {}", mx, my));
+        ui.label(format!("World x: {} y: {}", world_xy.x, world_xy.y));
+        ui.label(format!("Screensize: {}", screen_size));
+        ui.label(format!("Grid complete? {}", game_state.is_complete()));
+        ui.add(Slider::new(&mut debuggable_stuff.size_x, 1..=800).text("BG width"));
+        ui.add(Slider::new(&mut debuggable_stuff.size_y, 1..=800).text("BG height"));
+
+        ui.separator();
+        ui.label("grid even: ");
+        ui.color_edit_button_rgba_unmultiplied(&mut palette.grid_even);
+        ui.label("grid odd: ");
+        ui.color_edit_button_rgba_unmultiplied(&mut palette.grid_odd);
+        ui.label("background: ");
+        ui.color_edit_button_rgba_unmultiplied(&mut palette.background);
+        ui.label("cell_filled_in: ");
+        ui.color_edit_button_rgba_unmultiplied(&mut palette.cell_filled_in);
+        ui.label("cell_marked_user: ");
+        ui.color_edit_button_rgba_unmultiplied(&mut palette.cell_marked_user);
+        ui.label("cell_marked_game: ");
+        ui.color_edit_button_rgba_unmultiplied(&mut palette.cell_marked_game);
+        ui.label("cell_highlight: ");
+        ui.color_edit_button_rgba_unmultiplied(&mut palette.cell_highlight);
+        ui.label("cell_incorrect: ");
+        ui.color_edit_button_rgba_unmultiplied(&mut palette.cell_incorrect);
+        ui.label("palette.group_highlight: ");
+        ui.color_edit_button_rgba_unmultiplied(&mut palette.group_highlight);
+
+        ui.separator();
+        let before_level = debuggable_stuff.selected_level;
+        ComboBox::from_label("Load level").show_index(
+            ui,
+            &mut debuggable_stuff.selected_level,
+            levels.len(),
+            |i| levels[i],
+        );
+        if before_level != debuggable_stuff.selected_level {
+            let pbm = read_to_string(levels[debuggable_stuff.selected_level])
+                .expect("Could not load level");
+            let pbm: Pbm = pbm.parse().expect("level not in expected format");
+            *game_state = (&pbm).into();
+        }
+    });
 }
