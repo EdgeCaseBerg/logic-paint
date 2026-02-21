d02f13186e41a46596ec39918d9e71a76a08889b
peetseater <peetseater@gmail.com>
2026-02-21 11:12:06 -0500

    move editor_grids to own level

---
+++ src/bin/level_editor/editor_grids.rs
@@ -0,0 +1,125 @@
+use crate::LevelSettings;
+use crate::Level;
+use crate::{Vec2, vec2};
+use crate::Rect;
+use crate::Color;
+use crate::FrameContext;
+use crate::MouseButton;
+use std::path::PathBuf;
+
+pub struct EditorGrids {
+    pub pbm_grid: Vec<Vec<bool>>,
+    pub ppm_grid: Vec<Vec<[f32; 4]>>,
+    pub size: Vec2,
+    pub top_left: Vec2,
+}
+
+impl Default for EditorGrids {
+    fn default() -> EditorGrids {
+        let mut pbm_grid = Vec::with_capacity(20);
+        let mut ppm_grid = Vec::with_capacity(20);
+
+        for _ in 0..20 {
+            let mut pbm_row = Vec::with_capacity(20);
+            let mut ppm_row = Vec::with_capacity(20);
+            for _ in 0..20 {
+                pbm_row.push(false);
+                ppm_row.push([0.0, 0.0, 0.0, 1.0]);
+            }
+            pbm_grid.push(pbm_row);
+            ppm_grid.push(ppm_row);
+        }
+
+        EditorGrids {
+            pbm_grid,
+            ppm_grid,
+            size: vec2(400., 400.),
+            top_left: vec2(400., 120.), // [ 90 + 500 + 100 + 500 + 90  ]
+        }
+    }
+}
+
+impl EditorGrids {
+    pub fn ui(&mut self, frame_context: &mut FrameContext, level_settings: &mut LevelSettings) {
+        let gfx = &mut (frame_context.gfx);
+        let input = &mut (frame_context.input);
+        let left_mouse_pressed =
+            input.mouse_pressed(MouseButton::Left) || input.mouse_held(MouseButton::Left);
+        let right_mouse_pressed =
+            input.mouse_pressed(MouseButton::Right) || input.mouse_held(MouseButton::Right);
+        let (mx, my) = input.mouse_position();
+        let screen_size = gfx.screen_size();
+        let world_xy = gfx.camera().screen_to_world(Vec2::new(mx, my), screen_size);
+
+        let num_boxes_x = level_settings.width;
+        let num_boxes_y = level_settings.height; // TODO: maybe just always have a square
+        let gutter = 2.;
+        let cell_size = (self.size.x - (gutter + gutter * num_boxes_x as f32)) / num_boxes_x as f32;
+        let cell_size = Vec2::splat(cell_size);
+
+        let pbm_anchor = self.top_left;
+        gfx.rect()
+            .at(pbm_anchor)
+            .size(self.size)
+            .color(Color::WHITE);
+
+        let pbm_anchor = pbm_anchor + gutter;
+        for r in 0..num_boxes_y {
+            for c in 0..num_boxes_x {
+                let position = pbm_anchor + vec2(c as f32, r as f32) * (cell_size + gutter);
+
+                if Rect::new(position, cell_size).contains(world_xy) && left_mouse_pressed {
+                    self.pbm_grid[r][c] = true;
+                }
+                if Rect::new(position, cell_size).contains(world_xy) && right_mouse_pressed {
+                    self.pbm_grid[r][c] = false;
+                }
+                let color = if self.pbm_grid[r][c] {
+                    Color::RED
+                } else {
+                    Color::BLACK
+                };
+
+                gfx.rect().at(position).size(cell_size).color(color);
+            }
+        }
+
+        let ppm_anchor = pbm_anchor + vec2(400. + 50., 0.);
+        gfx.rect()
+            .at(ppm_anchor)
+            .size(self.size)
+            .color(Color::WHITE);
+
+        let ppm_anchor = ppm_anchor + gutter;
+        for r in 0..num_boxes_y {
+            for c in 0..num_boxes_x {
+                let position = ppm_anchor + vec2(c as f32, r as f32) * (cell_size + gutter);
+
+                if Rect::new(position, cell_size).contains(world_xy) && left_mouse_pressed {
+                    self.ppm_grid[r][c] = level_settings.current_color;
+                }
+                if Rect::new(position, cell_size).contains(world_xy) && right_mouse_pressed {
+                    self.ppm_grid[r][c] = [0.0, 0.0, 0.0, 1.0];
+                }
+                let rgb = self.ppm_grid[r][c];
+
+                gfx.rect()
+                    .at(position)
+                    .size(cell_size)
+                    .color(Color::new(rgb));
+            }
+        }
+    }
+}
+
+pub fn save_grid_as_level(level_settings: &LevelSettings, grids: &EditorGrids) -> Level {
+    let ppm = grids.into();
+    let pbm = grids.into();
+    let path: PathBuf = ["./levels", &level_settings.filename, ].iter().collect();
+    Level {
+        info: pbm,
+        image: ppm,
+        completed: false,
+        path: path.with_extension("level"),
+    }
+}
--- src/bin/level_editor/main.rs
+++ src/bin/level_editor/main.rs
@@ -1,11 +1,14 @@
 mod level_settings;
 mod ui_actions;
+mod editor_grids;
 
 use level_settings::LevelSettings;
+use editor_grids::{EditorGrids, save_grid_as_level};
 use crate::ui_actions::UiActions;
 
-// use logicpaint::netbpm;
-// use logicpaint::netppm;
+use logicpaint::netbpm::Pbm;
+use logicpaint::netppm::Ppm;
+use logicpaint::levels::Level;
 
 use egor::{
     app::{App, WindowEvent},
@@ -70,107 +73,3 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-struct EditorGrids {
-    pbm_grid: Vec<Vec<bool>>,
-    ppm_grid: Vec<Vec<[f32; 4]>>,
-    size: Vec2,
-    top_left: Vec2,
-}
-
-impl Default for EditorGrids {
-    fn default() -> EditorGrids {
-        let mut pbm_grid = Vec::with_capacity(20);
-        let mut ppm_grid = Vec::with_capacity(20);
-
-        for _ in 0..20 {
-            let mut pbm_row = Vec::with_capacity(20);
-            let mut ppm_row = Vec::with_capacity(20);
-            for _ in 0..20 {
-                pbm_row.push(false);
-                ppm_row.push([0.0, 0.0, 0.0, 1.0]);
-            }
-            pbm_grid.push(pbm_row);
-            ppm_grid.push(ppm_row);
-        }
-
-        EditorGrids {
-            pbm_grid,
-            ppm_grid,
-            size: vec2(400., 400.),
-            top_left: vec2(400., 120.), // [ 90 + 500 + 100 + 500 + 90  ]
-        }
-    }
-}
-
-impl EditorGrids {
-    fn ui(&mut self, frame_context: &mut FrameContext, level_settings: &mut LevelSettings) {
-        let gfx = &mut (frame_context.gfx);
-        let input = &mut (frame_context.input);
-        let left_mouse_pressed =
-            input.mouse_pressed(MouseButton::Left) || input.mouse_held(MouseButton::Left);
-        let right_mouse_pressed =
-            input.mouse_pressed(MouseButton::Right) || input.mouse_held(MouseButton::Right);
-        let (mx, my) = input.mouse_position();
-        let screen_size = gfx.screen_size();
-        let world_xy = gfx.camera().screen_to_world(Vec2::new(mx, my), screen_size);
-
-        let num_boxes_x = level_settings.width;
-        let num_boxes_y = level_settings.height; // TODO: maybe just always have a square
-        let gutter = 2.;
-        let cell_size = (self.size.x - (gutter + gutter * num_boxes_x as f32)) / num_boxes_x as f32;
-        let cell_size = Vec2::splat(cell_size);
-
-        let pbm_anchor = self.top_left;
-        gfx.rect()
-            .at(pbm_anchor)
-            .size(self.size)
-            .color(Color::WHITE);
-
-        let pbm_anchor = pbm_anchor + gutter;
-        for r in 0..num_boxes_y {
-            for c in 0..num_boxes_x {
-                let position = pbm_anchor + vec2(c as f32, r as f32) * (cell_size + gutter);
-
-                if Rect::new(position, cell_size).contains(world_xy) && left_mouse_pressed {
-                    self.pbm_grid[r][c] = true;
-                }
-                if Rect::new(position, cell_size).contains(world_xy) && right_mouse_pressed {
-                    self.pbm_grid[r][c] = false;
-                }
-                let color = if self.pbm_grid[r][c] {
-                    Color::RED
-                } else {
-                    Color::BLACK
-                };
-
-                gfx.rect().at(position).size(cell_size).color(color);
-            }
-        }
-
-        let ppm_anchor = pbm_anchor + vec2(400. + 50., 0.);
-        gfx.rect()
-            .at(ppm_anchor)
-            .size(self.size)
-            .color(Color::WHITE);
-
-        let ppm_anchor = ppm_anchor + gutter;
-        for r in 0..num_boxes_y {
-            for c in 0..num_boxes_x {
-                let position = ppm_anchor + vec2(c as f32, r as f32) * (cell_size + gutter);
-
-                if Rect::new(position, cell_size).contains(world_xy) && left_mouse_pressed {
-                    self.ppm_grid[r][c] = level_settings.current_color;
-                }
-                if Rect::new(position, cell_size).contains(world_xy) && right_mouse_pressed {
-                    self.ppm_grid[r][c] = [0.0, 0.0, 0.0, 1.0];
-                }
-                let rgb = self.ppm_grid[r][c];
-
-                gfx.rect()
-                    .at(position)
-                    .size(cell_size)
-                    .color(Color::new(rgb));
-            }
-        }
-    }
-}
