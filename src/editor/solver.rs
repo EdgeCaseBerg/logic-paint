83748bf7a9beaab80ef36640b1289079c14bc102
peetseater <peetseater@gmail.com>
2026-03-05 11:16:32 -0500

    Start working on solver line patterns

--- src/editor/solver.rs
+++ src/editor/solver.rs
@@ -13,3 +13,26 @@ pub fn editor_to_initial_state(level_settings: &LevelSettings, grids: &EditorGri
     let pbm: Pbm = (level_settings, grids).into();
     (&pbm).into()
 }
+
+type LinePattern = u32;
+
+// is_empty p == 0
+// is_full? p == u32::MAX
+
+pub fn generate_line_pattern(size: usize, groups: &[usize]) -> Vec<LinePattern> {
+    // TODO: Shift the bits around wahoo
+    vec![2147483648]
+}
+
+#[cfg(test)]
+mod pbm_tests {
+    use super::*;
+
+    #[test]
+    fn name() {
+        let patterns = generate_line_pattern(1, &[1]);
+        assert_eq!(patterns.len(), 1);
+        //2147483648 aka 10000000000000000000000000000000
+        assert_eq!(u32::MAX ^ (u32::MAX >> 1), patterns[0]);
+    }
+}
