03562ad13fb4d896a46e08d0f2d695c939aaa20c
peetseater <peetseater@gmail.com>
2026-03-05 10:29:42 -0500

    Love using already made Froms to transform data

--- src/editor.rs
+++ src/editor.rs
@@ -1,3 +1,4 @@
 pub mod editor_grids;
 pub mod editor_settings;
 pub mod editor_ui_actions;
+pub mod solver;
--- src/editor/solver.rs
+++ src/editor/solver.rs
@@ -1,5 +1,15 @@
+use crate::editor::editor_settings::LevelSettings;
+use crate::editor::editor_grids::EditorGrids;
+use crate::playstate::PlayState;
+use crate::netbpm::Pbm;
+
 pub enum SolvedState {
 	Unsolvable,
 	UniqueSolution,
 	MultipleSolutions,
+}
+
+pub fn editor_to_initial_state(level_settings: &LevelSettings, grids: &EditorGrids) -> PlayState {
+    let pbm: Pbm = (level_settings, grids).into();
+    (&pbm).into()
 }
