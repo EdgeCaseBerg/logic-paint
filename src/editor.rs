9f9705c4e65015916bfd88d75563e1304f50aa01
peetseater <peetseater@gmail.com>
2026-03-08 15:39:32 -0400

    Create solver_display

--- src/editor.rs
+++ src/editor.rs
@@ -2,3 +2,4 @@ pub mod editor_grids;
 pub mod editor_settings;
 pub mod editor_ui_actions;
 pub mod solver;
+pub mod solver_display;
---
+++ src/editor/solver_display.rs
@@ -0,0 +1,49 @@
+use crate::editor::editor_settings::LevelSettings;
+use crate::editor::editor_grids::EditorGrids;
+use crate::netbpm::Pbm;
+use crate::playstate::PlayState;
+use crate::editor::solver::TheMultiVerseOfLines;
+use crate::editor::solver::SolvedState;
+use egor::app::egui::Ui;
+
+
+pub struct SolverDisplay {
+    pub iterations: usize,
+    pub state: SolvedState,
+}
+
+impl Default for SolverDisplay {
+    fn default() -> Self {
+        Self {
+            iterations: 0,
+            state: SolvedState::Unsolvable,
+        }
+    }
+}
+
+impl SolverDisplay {
+    pub fn ui(&mut self, ui: &mut Ui) { 
+        ui.separator();
+        ui.heading("Level Correctness");
+        let state = match self.state {
+            SolvedState::UniqueSolution => "Good: No guesses needed!",
+            SolvedState::Unsolvable => "Bad: Unsolvable",
+            SolvedState::MultipleSolutions => "Bad: Guessing or multiple solutions found.",
+        };
+        ui.label(state);
+        ui.label(format!("Solved in {}", self.iterations));
+        ui.separator();
+    }
+
+    pub fn recompute(&mut self, level_settings: &LevelSettings, editor_grids: &EditorGrids) {
+        let pbm: Pbm = (level_settings, editor_grids).into();
+        let ps: PlayState = (&pbm).into();
+        let mut possibilities = TheMultiVerseOfLines::new(&ps);
+        // TODO remove
+        eprintln!("STATE: {possibilities}");
+        self.iterations = possibilities.collapse(); 
+        self.state = possibilities.state();
+    }
+}
+
+
